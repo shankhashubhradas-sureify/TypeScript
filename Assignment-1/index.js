d953c73bebc22ff847232ff63457328ac1a05b4b
shankhashubhradas-sureify <shankhadas@sureify.com>
2023-07-25 14:38:51 +0530

    remove asgmt-1

--- Assignment-1/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions_1 = require("./utils/functions");
-console.log("Add:", (0, functions_1.add)(2, 10));
-console.log("Sub:", (0, functions_1.subtract)(2, 10));
-console.log("Mul:", (0, functions_1.multiply)(10, 2));
-console.log("Div:", (0, functions_1.division)(10, 2));
